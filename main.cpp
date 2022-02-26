1ef5a94873227c1366118d4797ff94be354451f4
DanielKarczewicz <dkarczewicz.2000@gmail.com>
2022-02-26 20:52:42 +0100

    replaced std::cout with functions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+void hello() { std::cout << "hello there\n"; }
+
 struct A {
 	int m(int a, int b) { return a+b; }
 
@@ -9,8 +11,8 @@ int main() {
 	A *a = new A;
 	a = nullptr;
 	std::cout << a->m(3, 4) << '\n';
-	std::cout << "hello there\n";
-	std::cout << "hello there\n";
+	hello();
+	hello();
 	return 0;
 
 }
