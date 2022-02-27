c50f4847aed274dfc4904376564a40f14f174be1
DanielKarczewicz <dkarczewicz.2000@gmail.com>
2022-02-27 09:43:41 +0100

    added dangling pointer

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
-void hello() { std::cout << "hello there\n"; }
-
 struct A {
 	int m(int a, int b) { return a+b; }
 
@@ -17,18 +15,22 @@ struct C {
 	virtual int u(int x, int y) { return x+y; }
 };
 
+
 int main() {
 	A *a = new A;
 	a = nullptr;
 	std::cout << a->m(3, 4) << '\n';
-	hello();
-	hello();
 	B *b = new B(1);
 	b = nullptr;
-	std::cout << b->h(6) << '\n';
+	//std::cout << b->h(6) << '\n';
 	C *c = new C;
 	c = nullptr;
-	std::cout << c->u(9, 5) << '\n';
+	//std::cout << c->u(9, 5) << '\n';
+
+	int *u = new int(10);
+	delete u;
+	*u = 123;
+	std::cout << &u << '\n' << *u << '\n';
 	return 0;
 
 }
